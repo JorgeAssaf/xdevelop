688b6404605508fa8620224d4358ff62023da339
Jorge Assaf <jorgeassaf160@gmail.com>
2025-11-26 18:43:10 -0600

    fix: remove commented-out redirect logic for auth routes in proxy function

--- proxy.ts
+++ proxy.ts
@@ -6,9 +6,10 @@ export function proxy(request: NextRequest) {
   if (!token) {
     return NextResponse.redirect(new URL("/auth/sign-in", request.url))
   }
-  if (token && (request.nextUrl.pathname === "/auth/sign-in" || request.nextUrl.pathname === "/auth/sign-up")) {
-    return NextResponse.redirect(new URL("/users", request.url))
-  }
+  // if (token && (request.nextUrl.pathname === "/auth/sign-in" || request.nextUrl.pathname === "/auth/sign-up")) {
+  //   return NextResponse.redirect(new URL("/users", request.url))
+  // }
+  return NextResponse.next();
 }
 
 export const config = {
@@ -17,8 +18,6 @@ export const config = {
     "/",
     "/book",
     "/book/:path*",
-    "/dashboard",
-    "/dashbord/:path*",
     "/post",
     "/post/:path*",
     "/users",
